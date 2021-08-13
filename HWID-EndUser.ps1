9855e8c0577b7c0b5eeb722a34b0e3e29991accb
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2021-08-12 22:07:25 -0500

    Updated Delivery to include Try Statement with other Methods

--- HWID-EndUser.ps1
+++ HWID-EndUser.ps1
@@ -16,8 +16,31 @@ Get-WindowsAutoPilotInfo.ps1 -OutputFile ($Serial + ' AutoPilotHWID.csv')
 Compress-Archive C:\HWID\* -DestinationPath ('c:\HWID ' + $Serial + '.zip')
 
 ## O365 SMTP, Requires Creds to E-mail ##
+## Try Using Authentication method - Option 1 - https://docs.microsoft.com/en-us/exchange/mail-flow-best-practices/how-to-set-up-a-multifunction-device-or-application-to-send-email-using-microsoft-365-or-office-365 ##
+try {
 $creds = get-credential
 $subject = 'HWID ' + $Serial
 $attachment = 'C:\HWID ' + $serial + '.zip'
 Send-MailMessage -Credential $creds -SmtpServer smtp.office365.com -Port 587 -usessl -From source@customer.com -To destination@customer.com -Subject $subject -Body HWID -Attachments $attachment  -Priority High -DeliveryNotificationOption OnSuccess, OnFailure
+}
+## Else, Try unauthenticated - Option 2 - https://docs.microsoft.com/en-us/exchange/mail-flow-best-practices/how-to-set-up-a-multifunction-device-or-application-to-send-email-using-microsoft-365-or-office-365 ##
+catch [System.Net.WebException],[System.IO.IOException] {
+$subject = 'HWID ' + $Serial
+$attachment = 'C:\HWID ' + $serial + '.zip'
+Send-MailMessage -SmtpServer customer-com.mail.protection.outlook.com -Port 25 -usessl -From source@customer.com -To destination@customer.com -Subject $subject -Body HWID -Attachments $attachment  -Priority High -DeliveryNotificationOption OnSuccess, OnFailure
+} ## Else, Open Up New Outlook E-mail Message with attachment with existing profile ##
+catch {
+$Outlook = New-Object -ComObject Outlook.Application
+$Mail = $Outlook.CreateItem(0)
+$Mail.To = "destination@customer.com"
+$Mail.Subject = "HWID Hash"
+$Mail.Body ="HWID Hash"
+$Mail.Attachments.Add($attachment)
+$Mail.Display()
+$Mail.Send()
+}
 ## End of Workstation Script ##
+
+
+
+
